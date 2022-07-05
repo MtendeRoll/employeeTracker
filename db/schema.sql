a5abdbc5510241331fcbb1e10b9cf373abc6a0b1
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-07-05 12:45:53 -0400

    updated schema tables and database

--- db/schema.sql
+++ db/schema.sql
@@ -3,34 +3,25 @@ CREATE DATABASE staff_db;
 
 USE staff_db;
 
-CREATE TABLE department (
-id INT PRIMARY KEY,
-name VARCHAR(30)
-
-FOREIGN KEY (id)
-REFERENCES role(department_id)
-ON DELETE SET NULL
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-id INT PRIMARY KEY,
-title VARCHAR(30),
-salary DECIMAL,
-department_id INT
-
-FOREIGN KEY (id)
-REFERENCES employee(role_id)
-ON DELETE SET NULL
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(15,2) NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE employee (
-id INT PRIMARY KEY,
-first_name VARCHAR(30),
-last_name VARCHAR(30),
-role_id INT,
-manager_id INT
-
-FOREIGN KEY (id)
-REFERENCES employee(manager_id)
-ON DELETE SET NULL
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
