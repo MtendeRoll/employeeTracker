83d70613d4dec2f11fc469f9319c07a40e71ed0c
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-07-07 11:35:34 -0400

    updated seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,56 +1,27 @@
---department seeds
 INSERT INTO department (name)
-VALUES ("Sales");
+VALUES
+("Sales"),
+("Engineering"),
+("Finance"),
+("Legal");
 
-INSERT INTO department (name)
-VALUES ("Engineering");
-
-INSERT INTO department (name)
-VALUES ("Finance");
-
-INSERT INTO department(name)
-VALUES("Legal");
-
---role seeds
-INSERT INTO roles (title, salary, department_id)
-VALUES("Salesperson", 80000, 1);
 
 INSERT INTO roles (title, salary, department_id)
-VALUES("Lead Engineer", 150000, 2);
-
-INSERT INTO roles (title, salary, department_id)
-VALUES("Software Engineer", 120000, 2);
-
-INSERT INTO roles (title, salary, department_id)
-VALUES("Account Manager", 160000, 3);
-
-INSERT INTO roles (title, salary, department_id)
-VALUES("Accountant", 125000, 3);
-
-INSERT INTO roles (title, salary, department_id)
-VALUES("Legal Team Lead", 250000, 4);
-
-INSERT INTO roles (title, salary, department_id)
-VALUES("Lawyer", 190000, 4);
-
---employee seeds
-INSERT INTO employee (first_name, last_name, roles_id, manager_id)
-VALUES("Mike", "Chan", 1, 1);
-
-INSERT INTO employee (first_name, last_name, roles_id)
-VALUES("Ashley", "Rodriguez", 2);
+VALUES
+("Salesperson", 80000, 1),
+("Lead Engineer", 150000, 2),
+("Software Engineer", 120000, 2),
+("Account Manager", 160000, 3),
+("Accountant", 125000, 3),
+("Legal Team Lead", 250000, 4),
+("Lawyer", 190000, 4);
 
 INSERT INTO employee (first_name, last_name, roles_id, manager_id)
-VALUES("Kevin", "Tupik", 2, 2);
-
-INSERT INTO employee (first_name, last_name, roles_id)
-VALUES("Kunal", "Sing", 3);
-
-INSERT INTO employee (first_name, last_name, roles_id, manager_id)
-VALUES("Malia", "Brown", 3, 3);
-
-INSERT INTO employee (first_name, last_name, roles_id)
-VALUES("Sarah", "Lourd", 4);
-
-INSERT INTO employee (first_name, last_name, roles_id, manager_id)
-VALUES("Tom", "Allen", 4, 4);
+VALUES
+("Mike", "Chan", 1, 1),
+("Ashley", "Rodriguez", 2),
+("Kevin", "Tupik", 2, 2),
+("Kunal", "Sing", 3),
+("Malia", "Brown", 3, 3),
+("Sarah", "Lourd", 4),
+("Tom", "Allen", 4, 4);
